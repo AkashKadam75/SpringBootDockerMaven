e935905c2688d7695966dd8d024ff10d2f10c365
AkashKadam75 <kadamakash75@gmail.com>
2018-12-31 16:21:08 -0700

    SpringBoot CRUD with jenkinsfile

--- src/main/java/demo/service/PersonService.java
+++ src/main/java/demo/service/PersonService.java
@@ -1,8 +1,6 @@
 package demo.service;
 
 import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.stream.Stream;
 
 import org.springframework.stereotype.Service;
 
@@ -13,22 +11,22 @@ public class PersonService {
 	ArrayList<Person> persons = new ArrayList<Person>();
 	public PersonService() {
 		Person p = new Person();
-		p.setId("1");
+		p.setId(1l);
 		p.setAge(21);
 		p.setFirstName("Steve");
 		p.setLastName("Smith");
 		persons.add(p);
 
 		p = new Person();
-		p.setId("2");
+		p.setId(2l);
 		p.setAge(25);
 		p.setFirstName("Tom");
 		p.setLastName("Brown");
 		persons.add(p);
 	}
-	public Person getPerson(String id) {
+	public Person getPerson(Long id) {
 		for(Person person:persons) {
-			if(person.getId().equalsIgnoreCase(id)) return person;
+			if(person.getId() == id) return person;
 		}
 	    return null;
 	}
