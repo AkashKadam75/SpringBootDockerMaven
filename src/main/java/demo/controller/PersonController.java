7e1b0997a1e97ebb2cb5e296af7cb278b4154193
AkashKadam75 <kadamakash75@gmail.com>
2019-01-01 03:11:07 -0700

    final stable ci build fix

--- src/main/java/demo/controller/PersonController.java
+++ src/main/java/demo/controller/PersonController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import demo.model.Person;
 import demo.repository.PersonRepository;
 
+@CrossOrigin
 @RestController
 public class PersonController {
 	
